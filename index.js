fc30c029ea48170153ee5d5db18805e15373568b
salasaa <akhirudinsalasa.gc@gmail.com>
2025-06-20 00:26:29 +0700

    feat: add initial HTML data in JavaScript

---
+++ index.js
@@ -0,0 +1,120 @@
+const contacts = [
+  {
+    id: 1,
+    name: "John Doe",
+    Age: 30,
+    email: "john@example.com",
+    phone: "123-456-7890",
+    website: "https://johndoe.com",
+    lebels: [
+      {
+        id: 1,
+        name: "Friend",
+        color: "bg-blue-500",
+        textColor: "text-white",
+      },
+      {
+        id: 2,
+        name: "Work",
+        color: "bg-green-500",
+        textColor: "text-white",
+      },
+    ],
+    address: {
+      street: "456 Elm St",
+      city: "Othertown",
+      state: "NY",
+      country: "USA",
+    },
+    isFavorited: true,
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    Age: 25,
+    email: "jane@example.com",
+    phone: "987-654-3210",
+    website: "https://janesmith.com",
+    lebels: [
+      {
+        id: 1,
+        name: "Friend",
+        color: "bg-blue-500",
+        textColor: "text-white",
+      },
+      {
+        id: 2,
+        name: "Work",
+        color: "bg-green-500",
+        textColor: "text-white",
+      },
+    ],
+    address: {
+      street: "123 Main St",
+      city: "Anytown",
+      state: "CA",
+      country: "USA",
+    },
+    isFavorited: true,
+  },
+  {
+    id: 3,
+    name: "Alice Johnson",
+    Age: 28,
+    email: "alice@example.com",
+    phone: "555-123-4567",
+    website: "https://alicejohnson.com",
+    lebels: [
+      {
+        id: 1,
+        name: "Friend",
+        color: "bg-blue-500",
+        textColor: "text-white",
+      },
+      {
+        id: 2,
+        name: "Work",
+        color: "bg-green-500",
+        textColor: "text-white",
+      },
+    ],
+    address: {
+      street: "789 Oak St",
+      city: "Sometown",
+      state: "TX",
+      country: "USA",
+    },
+    isFavorited: false,
+  },
+  {
+    id: 4,
+    name: "Bob Brown",
+    Age: 35,
+    email: "bob@example.com",
+    phone: "555-987-6543",
+    website: "https://bobbrown.com",
+    lebels: [
+      {
+        id: 1,
+        name: "Friend",
+        color: "bg-blue-500",
+        textColor: "text-white",
+      },
+      {
+        id: 2,
+        name: "Work",
+        color: "bg-green-500",
+        textColor: "text-white",
+      },
+    ],
+    address: {
+      street: "321 Pine St",
+      city: "Yourtown",
+      state: "FL",
+      country: "USA",
+    },
+    isFavorited: true,
+  },
+];
+
+console.log();
