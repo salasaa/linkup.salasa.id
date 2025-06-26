eb2bebfaf0fe23e715e58e8b0f6456a50f5c9206
salasaa <akhirudinsalasa.gc@gmail.com>
2025-06-26 13:23:50 +0700

    feat: add icon, update home and add new function in index.js

--- index.js
+++ index.js
@@ -1,120 +1,94 @@
 const contacts = [
   {
     id: 1,
-    name: "John Doe",
-    Age: 30,
-    email: "john@example.com",
-    phone: "123-456-7890",
-    website: "https://johndoe.com",
-    lebels: [
-      {
-        id: 1,
-        name: "Friend",
-        color: "bg-blue-500",
-        textColor: "text-white",
-      },
-      {
-        id: 2,
-        name: "Work",
-        color: "bg-green-500",
-        textColor: "text-white",
-      },
-    ],
-    address: {
-      street: "456 Elm St",
-      city: "Othertown",
-      state: "NY",
-      country: "USA",
-    },
+    firstName: "Salasa",
+    lastName: "id",
+    companyName: "Tech Solutions",
+    jobTitle: "Software Engineer",
+    email: "salasa@example.com",
+    phone: "+62 123-456-7890",
+    createdAt: new Date(),
+    website: "https://salasa.id",
     isFavorited: true,
   },
   {
     id: 2,
-    name: "Jane Smith",
-    Age: 25,
-    email: "jane@example.com",
-    phone: "987-654-3210",
-    website: "https://janesmith.com",
-    lebels: [
-      {
-        id: 1,
-        name: "Friend",
-        color: "bg-blue-500",
-        textColor: "text-white",
-      },
-      {
-        id: 2,
-        name: "Work",
-        color: "bg-green-500",
-        textColor: "text-white",
-      },
-    ],
-    address: {
-      street: "123 Main St",
-      city: "Anytown",
-      state: "CA",
-      country: "USA",
-    },
+    firstName: "Pramudia",
+    lastName: "Adi",
+    companyName: "Business Corp",
+    jobTitle: "Project Manager",
+    email: "pramudia@example.com",
+    phone: "+62 987-654-3210",
+    createdAt: new Date("06-26-2025"),
+    website: "https://pramudia.com",
     isFavorited: true,
   },
   {
     id: 3,
-    name: "Alice Johnson",
-    Age: 28,
-    email: "alice@example.com",
-    phone: "555-123-4567",
-    website: "https://alicejohnson.com",
-    lebels: [
-      {
-        id: 1,
-        name: "Friend",
-        color: "bg-blue-500",
-        textColor: "text-white",
-      },
-      {
-        id: 2,
-        name: "Work",
-        color: "bg-green-500",
-        textColor: "text-white",
-      },
-    ],
-    address: {
-      street: "789 Oak St",
-      city: "Sometown",
-      state: "TX",
-      country: "USA",
-    },
+    firstName: "Aysha",
+    lastName: "Shifa",
+    companyName: "Creative Agency",
+    jobTitle: "UX Designer",
+    email: "aysha@example.com",
+    phone: "+62 555-123-4567",
+    website: "https://ayshashifa.com",
+    createdAt: new Date().toLocaleString(),
     isFavorited: false,
   },
   {
     id: 4,
-    name: "Bob Brown",
-    Age: 35,
-    email: "bob@example.com",
-    phone: "555-987-6543",
-    website: "https://bobbrown.com",
-    lebels: [
-      {
-        id: 1,
-        name: "Friend",
-        color: "bg-blue-500",
-        textColor: "text-white",
-      },
-      {
-        id: 2,
-        name: "Work",
-        color: "bg-green-500",
-        textColor: "text-white",
-      },
-    ],
-    address: {
-      street: "321 Pine St",
-      city: "Yourtown",
-      state: "FL",
-      country: "USA",
-    },
+    firstName: "Daniyal",
+    lastName: "Said",
+    companyName: "Tesla",
+    jobTitle: "Content Strategist",
+    email: "daniyal@example.com",
+    phone: "+62 555-987-6543",
+    website: "https://daniyalsaid.com",
+    createdAt: new Date().toLocaleDateString(),
     isFavorited: true,
   },
 ];
 
-console.log();
+console.log(contacts);
+
+/* Get all contacts */
+function getAllContacts() {
+  for (let contact of contacts) {
+    console.log(
+      contact.firstName,
+      contact.lastName,
+      contact.phone,
+      contact.createdAt
+    );
+  }
+}
+
+getAllContacts();
+
+/* Get contact by job title */
+// function getContact() {
+//   const jobTitle = contacts[2];
+//   return jobTitle;
+// }
+// const contact = getContact();
+// console.log(contact);
+
+// let addContact = [
+//   {
+//     id: 5,
+//     firstName: "John",
+//     lastName: "Doe",
+//     companyName: "New Company",
+//     jobTitle: "Developer",
+//     email: "john.doe@example.com",
+//     phone: "+62 123-456-7890",
+//     createdAt: new Date("06-26-2025"),
+//     website: "https://johndoe.com",
+//     isFavorited: false,
+//   },
+// ];
+// function addNewContact(newContact) {
+//   contacts.push(newContact);
+// }
+// addNewContact(addContact[0]);
+// console.log(contacts);
