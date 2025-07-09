9c4f3fbd40ce9b5651ce7a4c182d59221620480f
salasaa <akhirudinsalasa.gc@gmail.com>
2025-07-10 01:15:59 +0700

    fix: update API endpoint

--- fetch.js
+++ fetch.js
@@ -1,6 +1,8 @@
 async function getContacts() {
   // Fetch contacts from the API
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const response = await fetch(
+    "https://my-json-server.typicode.com/salasaa/json-server-online"
+  );
   // condition testing
   const users = await response.json();
   console.log(users);
